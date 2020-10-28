cc7d6a9c151109034d4d9ea8a1e2fe52a786e769
Brandon O <b.ondayko@gmail.com>
2020-10-28 13:29:21 -0500

    The day is now dynamiclly displayed and updated at the top of the page

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,20 @@
-console.log("hello");
+//Display the current day
+var currentDay = function() {
+    //date
+    var date = moment().local().format("ddd, h: mA").toString();
+    //display date
+    $("#currentDay")
+    .text("Today Is " + date);
+    
+    //update date
+    setInterval(function() {
+    var dateUpdated = moment().local().format("ddd, h: mA").toString();
+    $("#currentDay")
+    .text("Today Is " + dateUpdated);
+    console.log(dateUpdated);
+    }, 5000);
+}
+
+
+//function calls
+currentDay();
